88074e61b581279e7cefc8beb4aa3c8d3b1f7b37
Timothy Pavlushik <49930425+LemonPi314@users.noreply.github.com>
2021-03-11 15:28:36 -0800

    Build package files.

--- build/lib/pyconsole/example.py
+++ build/lib/pyconsole/example.py
@@ -1,7 +1,10 @@
 from pyconsole import *
 import time
 
-SetLogLevel("all")
+# Clear the log file
+Logger.ClearLog()
+# Log everything into the log file
+Logger.SetLogLevel(Logger.Levels.everything)
 
 PrintMessage("This is an error message.", "ERROR")
 PrintMessage("This is a warning message.", "WARNING")
@@ -9,22 +12,24 @@ PrintMessage("This is a success message.", "SUCCESS")
 PrintMessage("This is an info message.", "INFO")
 PrintMessage("This is a generic message.")
 PrintMessage("This is a message with a custom prefix.", "Custom Prefix")
-PrintMessage("This is a message with a custom color.", "none", Color.BrightCyan)
-PrintMessage("You can combine custom prefixes and colors.", "Example", Color.White, Color.BrightMagenta)
-PrintMessage("You can even override preset message colors.", "ERROR", Color.White, Color.Blue)
+PrintMessage("This is a message with a custom color.", messageColor=Color.BrightCyan)
+PrintMessage("You can combine custom prefixes and colors.", "Example", prefixColor=Color.BrightMagenta)
+PrintMessage("You can even override preset message colors.", "ERROR", prefixColor=Color.Blue)
 PrintMessage("You can set the colors for the message and prefix separately.", "Example", Color.BrightCyan, Color.Yellow)
-PrintMessage("For even more customizability, you can choose whether to color only the prefix,", "INFO", Color.White, Color.BrightBlue)
-PrintMessage("Or the brackets as well.", "INFO", Color.White, Color.BrightBlue, True)
-PrintMessage("This message has been logged in the log file.", "INFO", Color.White, Color.White, False, True)
+PrintMessage("For even more customizability, you can choose whether to color only the prefix,", "INFO", prefixColor=Color.BrightBlue)
+PrintMessage("Or the brackets as well.", "INFO", prefixColor=Color.BrightBlue, colorBrackets=True)
+PrintMessage("This message has been logged in the log file.", "INFO", forceLog=True)
 PrintMessage("\n")
 
-ProgressBar(0, 50, "Example progress bar:", "Complete", 50)
-for item in range(0, 50):
+# Create an instance of the progress bar
+progressBar = ProgressBar(prefix="Example progress bar:", length=50)
+# Update the progress bar every 100 milliseconds, 100 times
+for item in range(1, 101):
     time.sleep(0.1)
-    ProgressBar(item + 1, 50, "Example progress bar:", "Complete", 50)
+    progressBar.update(item)
 
 PrintMessage("\n")
 UserInput("This is a prompt for input. ")
 UserInput("The prompts can be colored. ", Color.BrightYellow)
 UserInput("As well as the user's input. ", Color.BrightGreen, Color.Cyan)
-UserInput("Press ENTER to exit the program. ")
+UserInput("Press enter to exit the program. ")
