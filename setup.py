fc81602d580b4fc3b3879345b895d8167eb4cb82
Timothy Pavlushik <49930425+LemonPi314@users.noreply.github.com>
2021-03-30 23:44:10 -0700

    Added alternate function and class names, updated docstrings, code cleanup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyconsole",
-    version="0.4",
+    version="0.6",
     author="Duplexes and LemonPi314",
     author_email="49930425+LemonPi314@users.noreply.github.com",
     description="Some tools for printing with colors in the console, as well as logging functions.",
